4ea5abe5220b73209783ed212122114cb6c3f6f7
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-04-27 23:29:12 +0530

    Update main.js

--- init/main.js
+++ init/main.js
@@ -1,17 +1,23 @@
-const mongoose = require('mongoose');
-const initData = require('./data');
-const Listings = require('../models/schema');
+const mongoose = require("mongoose");
+const dataDb = require("./data");
+const HomeKey = require("../models/schema")
 
 main()
-    .then(() => { console.log('database is connected') })
-    .catch(err => { console.log(err) })
+    .then(() => {
+        console.log('database is connected')
+    }).catch(err => {
+        console.log(err)
+    });
+
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/WANDER');
+    await mongoose.connect('mongodb://127.0.0.1:27017/final');
 }
-const intDb = async() =>{
-    await Listings.deleteMany({});
-    await Listings.insertMany(initData.data);
+
+const MainData = async() =>{
+    await HomeKey.deleteMany({});
+    await HomeKey.insertMany(dataDb.data);
     console.log('it is start')
 }
 
-intDb();
+const hello = MainData();
+console.log(hello);
