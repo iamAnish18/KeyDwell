9b4ea08cce3d9b0c312093d542ce8e93bac65f60
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-04-28 23:57:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,71 +1,78 @@
-const express = require("express");
+const express = require('express');
 let app = express();
 const port = 8080;
-const path = require("path");
-const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
-
-//Mongoose
+const path = require('path');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
-const HomeKey = require("./models/schema")
+const HomeKey = require('./models/schema');
+const methodOverride = require("method-override");
 
 main()
-    .then(() => console.log(`Database is connected`))
-    .catch((err) => console.log(err));
+    .then(() => {
+        console.log('Database is connected')
+    })
+    .catch(err => {
+        console.log(err)
+    });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/final');
+    await mongoose.connect('mongodb://127.0.0.1:27017/homeKey');
 }
-
 //Middleware
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname , "public")));
 app.use(express.urlencoded({extended : true}));
-app.use(methodOverride("_method"));
 app.engine("ejs" , ejsMate);
+app.use(methodOverride("_method"));
 
-//Index route
-app.get("/HomeKey" , async (req,res)=>{
-    let HomeList = await HomeKey.find({});
-    res.render("index/list.ejs" , {HomeList})
+//Server
+
+//index route
+app.get("/homeKey" , async(req,res)=>{//pass
+   let insertdata = await HomeKey.find({});
+   res.render('index/list.ejs' , {insertdata});
 });
-//Creat Route
-app.get("/HomeKey/add" , (req,res)=>{
-    res.render("index/create.ejs")
-})
-app.post("/HomeKey" , async (req,res)=>{
+
+//Create route
+app.get("/homeKey/add" , (req,res)=>{//pass
+    res.render('index/add.ejs');
+});
+app.post("/homeKey" , async(req,res)=>{
     const newHome = new HomeKey(req.body.newhome);
     await newHome.save();
-    res.redirect("/HomeKey");
+    res.redirect('/homeKey');
+});
+//edit route
+app.get("/homeKey/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let editHome = await HomeKey.findById(id);
+    res.render('index/edit.ejs', { editHome });
 });
 
-//edit route
-app.get("/HomeKey/:id/edit" , async(req,res)=>{
-    let {id} = req.params;
-    const newHomeid = await HomeKey.findByIdAndUpdate(id)
-    res.render("index/edit.ejs" , {newHomeid});
-})
+// PUT to update the data
+app.put("/homeKey/:id", async (req, res) => { 
+    let { id } = req.params;
+    await HomeKey.findByIdAndUpdate(id, { ...req.body.editHome});
+    res.redirect(`/homeKey/${id}`);
+});
 
-app.put("/HomeKey/:id" , async (req,res)=>{
+
+//delete route
+app.delete("/homeKey/:id" , async(req,res)=>{
     let {id} = req.params;
-    await HomeKey.findByIdAndUpdate(id , {...req.body.newHomeid});
-    res.redirect(`/Homekey/${id}`);
+    let deleteHome = await HomeKey.findByIdAndDelete(id);
+    console.log(deleteHome);
+    res.redirect('/homeKey');
 })
-//Delete route
-app.delete("/HomeKey/:id" , async(req,res)=>{
+//show route
+app.get("/homeKey/:id" ,async (req,res)=>{
     let {id} = req.params;
-    let deleteitem = await HomeKey.findByIdAndDelete(id);
-    console.log(deleteitem);
-    res.redirect("/HomeKey")
-})
-//Show route
-app.get("/HomeKey/:id" , async(req,res)=>{
-    let {id} = req.params;
-    const HomeList = await HomeKey.findById(id);
-    res.render("index/detail.ejs" , {HomeList});
+    let showData = await HomeKey.findById(id);
+    res.render('index/show.ejs' , {showData});
 })
 app.listen(port , ()=>{
-    console.log(`Server is running ${port}`)
+    console.log(`Server is running on ${port}`)
 })
