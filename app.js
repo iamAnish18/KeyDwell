6d1490ca573011a9bf18cf23bfd0cc5ce77b24fd
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-04-30 00:10:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const HomeKey = require('./models/schema');
 const methodOverride = require("method-override");
+const wrapAsync = require("./unit/wrap")
+const ExpressError = require("./unit/expressError");
+const {newhomeSchema} = require("./joi.js");
 
 main()
     .then(() => {
@@ -19,60 +22,80 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/homeKey');
 }
 //Middleware
-app.set("view engine" , "ejs");
-app.set("views" , path.join(__dirname , "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname , "public")));
-app.use(express.urlencoded({extended : true}));
-app.engine("ejs" , ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
 
 //Server
 
 //index route
-app.get("/homeKey" , async(req,res)=>{//pass
-   let insertdata = await HomeKey.find({});
-   res.render('index/list.ejs' , {insertdata});
-});
+app.get("/homeKey",wrapAsync( async (req, res) => {//pass
+    let insertdata = await HomeKey.find({});
+    res.render('index/list.ejs', { insertdata });
+}));
 
 //Create route
-app.get("/homeKey/add" , (req,res)=>{//pass
+app.get("/homeKey/add", (req, res) => {//pass
     res.render('index/add.ejs');
 });
-app.post("/homeKey" , async(req,res)=>{
+app.post("/homeKey", wrapAsync(async (req, res) => {
+    let result = newhomeSchema.validate(req.body);
+    console.log(result);
+    if(result.error){
+        throw new Error(400 , "validations error");
+    }
     const newHome = new HomeKey(req.body.newhome);
     await newHome.save();
     res.redirect('/homeKey');
-});
+}));
 //edit route
-app.get("/homeKey/:id/edit", async (req, res) => {
+app.get("/homeKey/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let editHome = await HomeKey.findById(id);
     res.render('index/edit.ejs', { editHome });
-});
+}));
 
 // PUT to update the data
-app.put("/homeKey/:id", async (req, res) => { 
+app.put("/homeKey/:id", wrapAsync(async (req, res) => {
+    if(!req.body.editHome){
+        throw new ExpressError(400 , "Enter Valid data")
+    }
     let { id } = req.params;
-    await HomeKey.findByIdAndUpdate(id, { ...req.body.editHome});
+    await HomeKey.findByIdAndUpdate(id, { ...req.body.editHome });
     res.redirect(`/homeKey/${id}`);
-});
+}));
 
 
 //delete route
-app.delete("/homeKey/:id" , async(req,res)=>{
-    let {id} = req.params;
+app.delete("/homeKey/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deleteHome = await HomeKey.findByIdAndDelete(id);
     console.log(deleteHome);
     res.redirect('/homeKey');
-})
+}));
 //show route
-app.get("/homeKey/:id" ,async (req,res)=>{
-    let {id} = req.params;
+app.get("/homeKey/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let showData = await HomeKey.findById(id);
-    res.render('index/show.ejs' , {showData});
+    res.render('index/show.ejs', { showData });
+}));
+
+// app.all("*" , (req,res)=>{
+//     throw new ExpressError(404,"Page is not found");
+//     // next(new ExpressError(404,"Page is not found"))
+// });
+
+//error handling
+app.use(( err , req, res, next) => {
+    let {status=500,message="Something went wrong"} = err;
+    res.render("index/error.ejs" , {err});
+    // res.status(status).send(message);
 })
-app.listen(port , ()=>{
+app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
