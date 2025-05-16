0e66303df884954d9b810ab795a2dec93d3bb666
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-05-16 23:34:36 +0530

    Update main.js

--- init/main.js
+++ init/main.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
-const KeyDwellData = require('./data');
-const KeyDwellListing = require('../models/schema');
-
+const KeyDwellSchema = require('../models/schema');
+const KeyDwelldb = require('./data');
 main()
     .then(() => {
         console.log(`database is connected`)
-    }).catch(err => {
+    }).catch((err) => {
         console.log(err)
     });
-
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/KeyDwell');
 }
 
-let KeyDwellDb = async () => {
-    await KeyDwellListing.deleteMany({});
-    await KeyDwellListing.insertMany(KeyDwellData.data)
+let insert =async () =>{
+    await KeyDwellSchema.deleteMany({});
+    await KeyDwellSchema.insertMany(KeyDwelldb.data)
 }
-KeyDwellDb();
+
+insert();
