15b8a511ab3b9bb41caf06bb17a338e36bf536ab
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-05-17 23:26:16 +0530

    Update joi.js

--- joi.js
+++ joi.js
@@ -1,13 +1,13 @@
-const joi = require("joi");
+const Joi = require('joi');
 
-module.exports.newHomeSchema = Joi.object({
-    newhome : Joi.object({
+module.exports.keydataSchema = Joi.object({
+    keydata : Joi.object({
         title : Joi.string().required(),
         description : Joi.string().required(),
+        image : Joi.string().allow("" , null),
         price : Joi.number().required().min(0),
         email_id : Joi.string().required(),
         location : Joi.string().required(),
-        country : Joi.string().required(),
-        image: Joi.string().allow("" ,null)
+        country : Joi.string().required()
     }).required()
-})
+});
