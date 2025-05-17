bfdae3e927c82a4cbb6a4a0ec060246ed4212597
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-05-17 21:23:08 +0530

    Delete unit directory

--- unit/expressError.js
+++
@@ -1,9 +0,0 @@
-class Expresserror extends Error{
-    constructor(status,message){
-        super();
-        this.status = status;
-        this.message = message;
-    }
-}
-
-module.exports = Expresserror;
