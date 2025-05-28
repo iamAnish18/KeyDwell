2728ca7b0da16fcaa495252506cb1229912abfae
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-05-28 23:06:36 +0530

    Update keydwell.js

--- routes/keydwell.js
+++ routes/keydwell.js
@@ -4,7 +4,6 @@ const { keydataSchema } = require('../joi.js');
 const wrapAsync = require('../utily/wrapAsync.js');
 const KeyDwellSchema = require('../models/schema');
 const expressError = require('../utily/expressError.js');
-
 const keydataValidation = (req, res, next) => {
     let { error } = keydataSchema.validate(req.body.newhome);
     if (error) {
@@ -15,7 +14,7 @@ const keydataValidation = (req, res, next) => {
     }
 }
 // Index route
-route.get("/home",wrapAsync(async (req, res) => {
+route.get("/home", wrapAsync(async (req, res) => {
     let homedata = await KeyDwellSchema.find({});
     res.render('ejs/home.ejs', { homedata });
 }));
@@ -23,21 +22,27 @@ route.get("/home",wrapAsync(async (req, res) => {
 route.get("/create", (req, res) => {
     res.render('ejs/create.ejs')
 });
+// wrapAsync used better then try and catch functions
 route.post("/home", keydataValidation, wrapAsync(async (req, res) => {
     let createdata = new KeyDwellSchema(req.body.newhome);
-    console.log(createdata);
     await createdata.save();
+    req.flash("success", "New route create Successful");
     res.redirect('/Keydwell/home');
 }));
 // update route
 route.get("/:id/Update", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let updatedata = await KeyDwellSchema.findById(id);
+    if(!updatedata){
+        req.flash("error" ,"To finding the listing is deleted");
+        res.redirect('Keydwell/home');
+    }
     res.render('ejs/update.ejs', { updatedata });
 }));
 route.put("/show/:id", keydataValidation, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await KeyDwellSchema.findByIdAndUpdate(id, { ...req.body.updatedata });
+    req.flash("success", "Successfull Update your listing");
     res.redirect(`/Keydwell/show/${id}`);
 }));
 
@@ -46,12 +51,17 @@ route.delete("/show/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedata = await KeyDwellSchema.findByIdAndDelete(id);
     console.log(deletedata);
+    req.flash("success", "Successfull delete your listing")
     res.redirect('/Keydwell/home');
 }));
 
 route.get("/show/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let showdata = await KeyDwellSchema.findById(id).populate('reviews');
+    if(!showdata){
+        req.flash("error" , "To finding the listing is deleted");
+        res.redirect('Keydwell/home');
+    }
     res.render('ejs/show.ejs', { showdata });
 }));
 
