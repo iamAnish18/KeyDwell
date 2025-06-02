2c2dbbfd9730616ab08d2b089ac998d12a1b434d
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-06-02 15:41:28 +0530

    Update schema.js

--- models/schema.js
+++ models/schema.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
+const wrapAsync = require('../utily/wrapAsync');
 let keydata = new mongoose.Schema({
     title : {
         type : String
@@ -21,7 +23,22 @@ let keydata = new mongoose.Schema({
     },
     country : {
         type : String
+    },
+    reviews : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ],
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
     }
 });
+keydata.post("findOneAndDelete" ,wrapAsync(async()=>{
+    if(keydwells){
+        await Review.deleteMany({_id : {$in : keydwells.reviews}})
+    }
+}));
 const Keydwell = new mongoose.model("Keydwell" , keydata);
 module.exports = Keydwell;
