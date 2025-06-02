89b19b48d28fdca3b1d7f7372b5f6436a5360fad
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-06-02 15:42:24 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const route = express.Router({mergeParams : true});
 const Reviews = require('../models/review.js');
-const expressError = require('../utily/expressError.js');
-const {reviewsSchema } = require('../joi.js');
 const wrapAsync = require('../utily/wrapAsync.js');
 const KeyDwellSchema = require('../models/schema');
+const { checktoLogin , reviewsValidation ,isreviewAuthor} = require('../middleware.js');
 
-
-const reviewsValidation = (req, res, next) => {
-    let { error } = reviewsSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map(el => el.message).join(",")
-        throw new expressError(400, errMsg);
-    } else {
-        next();
-    }
-}
-
-route.post("/Keydwell/:id/reviews", reviewsValidation, async (req, res) => {
+route.post("/",checktoLogin, reviewsValidation, async (req, res) => {
     let hotelreview = await KeyDwellSchema.findById(req.params.id);
     let newreview = new Reviews(req.body.reviews);
-
+    newreview.author = req.user._id;
     hotelreview.reviews.push(newreview);
+    console.log(newreview);
     await hotelreview.save();
     await newreview.save();
     res.redirect(`/Keydwell/show/${hotelreview._id}`);
 });
 // reviews delete route
-route.delete("/Keydwell/:id/reviews/:reviewsId", wrapAsync(async (req, res) => {
+route.delete("/:reviewsId",isreviewAuthor,checktoLogin, wrapAsync(async (req, res) => {
     let { id, reviewsId } = req.params;
     await KeyDwellSchema.findByIdAndUpdate(id, { $pull: { reviews: reviewsId } });
     await Reviews.findByIdAndDelete(reviewsId);
